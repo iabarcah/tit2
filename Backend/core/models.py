93dd2d2ee5f90afc46dca602ac213008a1146243
iabarcah <ignacio.abarca@alumnos.uv.cl>
2022-12-06 15:27:37 -0300

    Cambio modelo backend

--- Backend/core/models.py
+++ Backend/core/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
+TIPO_EVENT = [
+    ('T','torneo'),
+    ('M','maraton'),
+    ('Z','zumba')
+]
 class Evento(models.Model):
     id_evento = models.AutoField(primary_key=True)
     nombre_evento = models.CharField(max_length=250)
-    tipo_evento = models.CharField(max_length=250)
+    tipo_evento = models.CharField(max_length=250, choices=TIPO_EVENT)
     cupo_evento = models.IntegerField()
     fecha_inicio_evento = models.DateField()
     fecha_fin_evento = models.DateField()
@@ -15,3 +19,4 @@ class Evento(models.Model):
     descripcion_evento = models.TextField()
     requisitos_evento = models.TextField()
     estado_evento = models.CharField(max_length=250)
+    URL_form = models.CharField(max_length=1500, default='')
